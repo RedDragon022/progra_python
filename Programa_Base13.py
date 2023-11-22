6f025ba7869e9eb906f4884cd262857b73b57b0b
RedDragon022 <147216552+RedDragon022@users.noreply.github.com>
2023-11-21 23:22:24 -0600

    Add files via upload

---
+++ Programa_Base13.py
@@ -0,0 +1,39 @@
+def convertir_base_21_a_9(numero_base_21):
+    # Convertir el número de base 21 a decimal
+    decimal = int(str(numero_base_21), 21)
+
+    # Convertir el decimal a base 9
+    resultado_base_9 = ""
+    while decimal > 0:
+        residuo = decimal % 9
+        if residuo > 9:
+            # Convertir dígitos mayores a 9 a letras
+            letra = chr(ord('A') + residuo - 10)
+            resultado_base_9 = letra + resultado_base_9
+        else:
+            resultado_base_9 = str(residuo) + resultado_base_9
+        decimal //= 9
+
+    return resultado_base_9
+
+# Función para cambiar números por letras en el rango 10-19
+def cambiar_numeros_por_letras(numero):
+    if 10 <= numero <= 19:
+        return chr(ord('A') + numero - 10)
+    else:
+        return str(numero)
+
+# Bucle principal
+while True:
+    numero_base_21 = input("Ingrese un número en base 21 (o 'salir' para terminar): ")
+
+    if numero_base_21.lower() == 'salir':
+        break
+
+    try:
+        resultado_base_9 = convertir_base_21_a_9(numero_base_21)
+        resultado_con_letras = ''.join([cambiar_numeros_por_letras(int(digito)) for digito in resultado_base_9])
+        print(f"El número {numero_base_21} en base 9 es: {resultado_con_letras}")
+    except ValueError:
+        print("Error: Ingrese un número válido en base 21.")
+
